afb740f07d7a343c8a8fb3da2b1369428a7c8bb7
anusha3202 <anushavigrahala3202@gmail.com>
2022-12-23 14:59:17 +0530

    First commit

---
+++ src/main/java/Anusha121/Anusha121/App.java
@@ -0,0 +1,13 @@
+package Anusha121.Anusha121;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
